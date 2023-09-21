9f3e4f6f30bc08c485ca2568a52eec1e1147b782
Rolando Duarte <rduarte@rolo-ixcomercio-lnx.ixcomercio.com>
2023-09-21 16:02:57 -0300

    Ajustes Thu Sep 21 04:02:57 PM -03 2023

---
+++ Assets/scripts/PersonajeController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+
+    public float velocidad;
+    public float fuerzaSalto;
+    private Rigidbody2D rigidbody;
+
+    private bool mirandoDerecha = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.ProcesarMovimiento();
+        this.ProcesarSalto();
+    }
+
+    void ProcesarSalto()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigidbody.AddForce(Vector2.up * this.fuerzaSalto , ForceMode2D.Impulse );
+        }
+    }
+
+    void ProcesarMovimiento()
+    {
+        float inputPositionAxisX = Input.GetAxis("Horizontal");
+        this.rigidbody.velocity = new Vector2(inputPositionAxisX * velocidad, this.rigidbody.velocity.y);
+        this.GestionarOrientacion(inputPositionAxisX);
+    }
+
+    void GestionarOrientacion(float inputMovimiento)
+    {
+        Debug.Log("valor this.mirandoDerecha -> " + this.mirandoDerecha);
+        if (
+            (this.mirandoDerecha == true && inputMovimiento < 0)
+             ||
+            (this.mirandoDerecha == false && inputMovimiento > 0)
+           )
+        {
+            Debug.Log("------------------------");
+            this.mirandoDerecha = !this.mirandoDerecha;
+            Debug.Log("Nuevo valor this.mirandoDerecha -> " + this.mirandoDerecha);
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            Debug.Log("------------------------");
+        }
+    }
+
+}
